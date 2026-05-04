2433ad90777a8b7e66660ffa711fc5117b71681d
liminalthinking <larryyap@gmail.com>
2026-05-04 11:08:14 +0800

    test: format check to ci

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,23 +1,20 @@
-import {describe, expect, test} from "vitest";
+import { describe, expect, test } from "vitest";
 // import { getAPIKey } from "./api/auth";
 // import { IncomingHttpHeaders } from "http";
-// import { MySqlDateTimeString } from "drizzle-orm/mysql-core";    
+// import { MySqlDateTimeString } from "drizzle-orm/mysql-core";
 
 const headers = {
-    "authorization" : "a four word string",
+  authorization: "a four word string",
 };
-    
+
 describe("headers", () => {
-    test("testing authorization values", () => {
-        expect(headers["authorization"]).toBe("a four word string")
-    });
-    
-    test("testing split auth", () => {
-        //const ApiKey = getAPIKey(headers);
-        const splitheaders = headers.authorization.split(" ")
-        expect(splitheaders[1]).toBe("four");
-        
-    });
+  test("testing authorization values", () => {
+    expect(headers["authorization"]).toBe("a four word string");
+  });
+
+  test("testing split auth", () => {
+    //const ApiKey = getAPIKey(headers);
+    const splitheaders = headers.authorization.split(" ");
+    expect(splitheaders[1]).toBe("four");
+  });
 });
-
-
